6c8aa697bc3b95bf19b1fa09b9b4c3dabbc250b4
Nguyen Thanh Long <nguyenlong.hcmc@gmail.com>
2022-09-18 01:07:29 +0700

    sign Out logic coding

---
+++ lib/presentation/features/profile/signOut.dart
@@ -0,0 +1,17 @@
+import 'package:provider/provider.dart';
+
+Future<void> _deleteCacheDir() async {
+  final cacheDir = await getTemporaryDirectory();
+
+  if (cacheDir.existsSync()) {
+    cacheDir.deleteSync(recursive: true);
+  }
+}
+
+Future<void> _deleteAppDir() async {
+  final appDir = await getApplicationSupportDirectory();
+
+  if (appDir.existsSync()) {
+    appDir.deleteSync(recursive: true);
+  }
+}
